9e599b6559a5a84bac09989d94232f6172894cf1
Zac Willmington <zac.willmington@gmail.com>
2018-09-09 14:54:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file
 
 function findMatching (drivers, driverName) {
-    const foundDrivers = drivers.filter(driver => driver === driverName);
+    const foundDrivers = drivers.filter(driver => driver === driverName || driver === driverName.lowerCase());
 
     return foundDrivers;
 }
