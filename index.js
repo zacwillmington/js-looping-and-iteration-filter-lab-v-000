38b742679d07c00c5a958dc2d58d503f7eebda95
Zac Willmington <zac.willmington@gmail.com>
2018-09-09 15:07:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ function findMatching (drivers, driverName) {
 }
 
 function fuzzyMatch (drivers, chars){
-    const matched = drivers.filter(matchChars (chars, drivers));
-    return matched;
+    const matched = drivers.filter(function (driver, chars) => { driver[0] + driver[1] == chars });
+    return matched
 }
 
 // function matchChars (chars, drivers) {
