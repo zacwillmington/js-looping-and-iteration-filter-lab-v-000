4ae71f70869decac38877e8e6791049e796d47d9
Zac Willmington <zac.willmington@gmail.com>
2018-09-09 15:09:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Code your solution in this file
 
 function findMatching (drivers, driverName) {
-    const foundDrivers = drivers.filter(driver => driver === driverName || driver === driverName.toLowerCase());
+    const foundDrivers = drivers.filter(function (driver, driverName) {driver === driverName || driver === driverName.toLowerCase()});
 
     return foundDrivers;
 }
 
 function fuzzyMatch (drivers, chars){
-    const matched = drivers.filter(function (driver, chars) => { return driver[0] + driver[1] === chars });
+    const matched = drivers.filter(function (driver, chars) { return driver[0] + driver[1] === chars });
     return matched
 }
 
